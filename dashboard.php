6e54aeb87859fea59ab0cf75574c2016d850d747
ytneS <90407905+ytneS@users.noreply.github.com>
2024-02-15 00:05:34 +0100

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Domovská stránka</title>
     <link rel="icon" href="IMG/logo2.png">
     <link rel="stylesheet" href="CSS/header.css" />
     <link rel="stylesheet" href="CSS/dashboard.css" />
