749b1d0e35c16ffcd1a10f91d4c4f5a90018f076
ytneS <90407905+ytneS@users.noreply.github.com>
2024-02-14 22:57:01 +0100

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,11 +1,14 @@
 <?php
-    $host = 'localhost';
-    $username = 'username';
-    $password = 'password';
-    $database = 'staff_nexus';
+$host = 'localhost';
+$username = 'username';
+$password = 'password';
+$database = 'staff_nexus';
 
-    $conn = mysqli_connect($host, $username, $password, $database);
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    }
+try {
+    $conn = new PDO("mysql:host=$host;dbname=$database", $username, $password);
+    // Set PDO to throw exceptions on error
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(PDOException $e) {
+    die("Connection failed: " . $e->getMessage());
+}
 ?>
