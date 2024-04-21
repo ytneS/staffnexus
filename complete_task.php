52aaf8cbf0c317bde684ab37df07c06dd5f841c9
ytneS <90407905+ytneS@users.noreply.github.com>
2024-04-21 21:46:56 +0200

    Update complete_task.php

--- complete_task.php
+++ complete_task.php
@@ -5,24 +5,20 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $taskId = $_POST["task_id"];
 
     try {
-        // Získání údajů o úkolu
         $selectQuery = "SELECT task, priority, description FROM todo WHERE task_id = ?";
         $stmtSelect = $conn->prepare($selectQuery);
         $stmtSelect->execute([$taskId]);
         $taskData = $stmtSelect->fetch(PDO::FETCH_ASSOC);
-        $stmtSelect->closeCursor();
 
-        // Přidání úkolu do completedtodo
         $insertQuery = "INSERT INTO completedtodo (task, priority, description) VALUES (?, ?, ?)";
         $stmtInsert = $conn->prepare($insertQuery);
         $stmtInsert->execute([$taskData["task"], $taskData["priority"], $taskData["description"]]);
 
-        // Úkol byl úspěšně přesunut do completedtodo, nyní jej odstraňte z todo
         $deleteQuery = "DELETE FROM todo WHERE task_id = ?";
         $stmtDelete = $conn->prepare($deleteQuery);
         $stmtDelete->execute([$taskId]);
 
-        echo "Task completed successfully";
+   
     } catch (PDOException $e) {
         echo "Error: " . $e->getMessage();
     }
