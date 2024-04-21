3d8648e92468116b1415cbb3865050765fce1193
ytneS <90407905+ytneS@users.noreply.github.com>
2024-04-21 21:45:56 +0200

    Update delete_task.php

--- delete_task.php
+++ delete_task.php
@@ -5,7 +5,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $taskId = $_POST["task_id"];
 
     try {
-        // Use the connection from connect.php
         $stmt = $conn->prepare("DELETE FROM todo WHERE task_id = :taskId");
         $stmt->bindParam(":taskId", $taskId, PDO::PARAM_INT);
 
