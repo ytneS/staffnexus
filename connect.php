7cc7c5eeffc58fd77c57a78f5030277300f51d62
ytneS <90407905+ytneS@users.noreply.github.com>
2024-04-21 21:46:29 +0200

    Update connect.php

--- connect.php
+++ connect.php
@@ -6,7 +6,6 @@ $database = 'staff_nexus';
 
 try {
     $conn = new PDO("mysql:host=$host;dbname=$database", $username, $password);
-    // Set PDO to throw exceptions on error
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch(PDOException $e) {
     die("Connection failed: " . $e->getMessage());
